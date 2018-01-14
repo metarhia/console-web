98b462c05534dea3f49f241a7592859cb0f544d1
kuvichkamaksim <kuvichkamaksim@gmail.com>
2018-01-14 16:24:40 +0200

    merge conflict solved

--- src/components/Table/TableComponent.js
+++ src/components/Table/TableComponent.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react'
-import ReactTable from 'react-table'
-import 'react-table/react-table.css'
-import './Table.css'
-import matchSorter from 'match-sorter'
+import React, { Component } from 'react';
+import ReactTable from 'react-table';
+import 'react-table/react-table.css';
+import './Table.css';
+import matchSorter from 'match-sorter';
 
 export default class TableComponent extends Component {
   render() {
@@ -11,56 +11,80 @@ export default class TableComponent extends Component {
         id: 1,
         name: 'Tanner Linsley',
         address: 'Ave Cesaria',
-        city: 'Rome'
+        city: 'Rome',
       }, {
         id: 2,
         name: 'Marcus Aurelius',
         address: 'Senat',
-        city: 'Rome'
+        city: 'Rome',
       }, {
         id: 3,
         name: 'Agent Smith',
         address: 'CIA',
-        city: 'Los-Angeles'
+        city: 'Los-Angeles',
       }, {
         id: 4,
         name: 'Typical Lumberjack',
         address: 'Bearland',
-        city: 'Toronto'
-      }
-    ]
+        city: 'Toronto',
+      },
+    ];
 
     const columns = [
       {
         Header: 'ID',
         accessor: 'id',
+        Filter: ({ filter, onChange }) =>
+          <input type="text"
+          onChange={event => onChange(event.target.value)}
+          style={{ width: '100%' }}
+          placeholder='...query...'
+        />,
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["id"] }),
-        filterAll: true
+          matchSorter(rows, filter.value, { keys: ['id'] }),
+        filterAll: true,
       }, {
         Header: 'Name',
         accessor: 'name',
+        Filter: ({ filter, onChange }) =>
+          <input type='text'
+          onChange={event => onChange(event.target.value)}
+          style={{ width: '100%' }}
+          placeholder='...query...'
+        />,
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["name"] }),
+          matchSorter(rows, filter.value, { keys: ['name'] }),
         filterAll: true,
-        Cell: props => <span className='number'>{props.value}</span>
+        Cell: props => <span className='number'>{props.value}</span>,
       }, {
         Header: 'Address',
         accessor: 'address',
+        Filter: ({ filter, onChange }) =>
+          <input type='text'
+          onChange={event => onChange(event.target.value)}
+          style={{ width: '100%' }}
+          placeholder='...query...'
+        />,
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["address"] }),
-        filterAll: true
+          matchSorter(rows, filter.value, { keys: ['address'] }),
+        filterAll: true,
       }, {
         Header: 'City',
         accessor: 'city',
+        Filter: ({ filter, onChange }) =>
+          <input type='text'
+          onChange={event => onChange(event.target.value)}
+          style={{ width: '100%' }}
+          placeholder='...query...'
+        />,
         filterMethod: (filter, rows) =>
-          matchSorter(rows, filter.value, { keys: ["city"] }),
-        filterAll: true
-      }
-    ]
+          matchSorter(rows, filter.value, { keys: ['city'] }),
+        filterAll: true,
+      },
+    ];
 
     return (
-      <div className="bottom">
+      <div className='bottom'>
         <ReactTable
           data={data}
           columns={columns}
@@ -69,10 +93,8 @@ export default class TableComponent extends Component {
           filterable
           defaultFilterMethod={(filter, row) =>
             String(row[filter.id]) === filter.value}
-          style={{
-          height: '100%'
-        }}/>
+        />
       </div>
-    )
+    );
   }
 }
