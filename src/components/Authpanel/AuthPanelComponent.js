165980c62397042be8b59b2221f548214a8e3d12
Dmitrii Mikhailov <31654782+Tariod@users.noreply.github.com>
2018-01-18 23:58:28 +0200

    fix: console layout (#15)

--- src/components/Authpanel/AuthPanelComponent.js
+++ src/components/Authpanel/AuthPanelComponent.js
@@ -1,17 +1,60 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../../assets/img/logo.png';
 import './AuthPanel.css';
 
-const AuthPanel = () => (
-  <div className='sidebar-auth-panel'>
-    <img className='sidebar-logo' src={logo}/>
-    <h2 className='sidebar-title'>Metarhia</h2>
-    <hr style={{
-      width: '80%',
-      opacity: 0.4
-    }}/>
-    <span>Username</span>
-  </div>
-);
+function LoginButton(props) {
+  return (
+    <button  className='sidebar-button' onClick={props.onClick}>
+      Sign In
+    </button>
+  );
+}
 
-export default AuthPanel;
+function LogoutButton(props) {
+  return (
+    <button className='sidebar-button' onClick={props.onClick}>
+      Sign Out
+    </button>
+  );
+}
+
+export default class AuthPanel extends Component {
+  constructor(props) {
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+
+  handleLoginClick() {
+    this.setState({isLoggedIn: true});
+  }
+
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
+
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+
+    return(
+      <div className='sidebar-auth-panel'>
+        <img className='sidebar-logo' src={logo}/>
+        <h2 className='sidebar-title'>Metarhia</h2>
+        <hr style={{
+          width: '80%',
+          border: '1px solid grey'
+        }}/>
+          {isLoggedIn ? (
+            <div>
+              <LogoutButton onClick={this.handleLogoutClick} />
+              <span className='sidebar-username'>Username</span>
+            </div>
+          ) : (
+            <LoginButton onClick={this.handleLoginClick} />
+          )}
+      </div>
+    )
+  }
+}
+
